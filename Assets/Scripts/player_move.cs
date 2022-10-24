0328fb537ee4431bff41c6aa70209301b25ad6c2
jnuneznunUOC <jnuneznun@uoc.edu>
2022-10-24 23:40:13 +0200

    lunes 24 noche

--- Assets/Scripts/player_move.cs
+++ Assets/Scripts/player_move.cs
@@ -7,9 +7,10 @@ public class player_move : MonoBehaviour
 
     private Rigidbody2D rig;
     private SpriteRenderer spritePersonaje;
-    float fuerzaSalto = 3f;
+    float fuerzaSalto = 4f;
     int numJump = 1;
     int useJump;
+    Animator anim;
 
     [SerializeField] private float velocidadPersonaje;
 
@@ -18,6 +19,7 @@ public class player_move : MonoBehaviour
         rig = GetComponent<Rigidbody2D>();
         spritePersonaje = GetComponent<SpriteRenderer>();
         useJump = 0;
+        anim = GetComponent<Animator>();
     }
 
     public void Jump()
@@ -29,18 +31,24 @@ public class player_move : MonoBehaviour
                 rig.AddForce(new Vector3(0f, fuerzaSalto), ForceMode2D.Impulse);
                 useJump++;
             }
+    
         }
+        
     }
 
     private void OnCollisionEnter2D(Collision2D obj)
     {
-        if (obj.collider.tag == "ground") {
+        if (obj.collider.tag == "ground")
+        {
             useJump = 0;
         }
+        
     }
     private void FixedUpdate()
     {
         Jump();
+        Attack();
+
         float velocidadInput = Input.GetAxisRaw("Horizontal");
         rig.velocity = new Vector2(velocidadInput * velocidadPersonaje, rig.velocity.y);
         if (velocidadInput < 0)
@@ -51,6 +59,31 @@ public class player_move : MonoBehaviour
         {
             spritePersonaje.flipX = false;
         }
+        if (rig.velocity.x !=0)
+        {
+            anim.SetBool("walk", true);
+        }
+        else {
+            anim.SetBool("walk",false);
+        }
+
     }
+    public void Attack()
+    {
+        if (Input.GetKey("l"))
+        {
+
+            anim.SetBool("atack", true);
+
+        }
+        else
+        {
+
+            anim.SetBool("atack", false);
+        }
+
+
+    }
+
 }
 
