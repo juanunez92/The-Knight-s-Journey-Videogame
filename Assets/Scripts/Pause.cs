12b52430b5e2471db73d3fe8e00d847d1cdf246f
jnuneznunUOC <jnuneznun@uoc.edu>
2022-11-09 03:12:18 +0100

    Miercoles 9
    Se rehace el escenario wood, aún falta terminar y hacer escenario enemigo final, Escenario Cave y configuración enemigos

---
+++ Assets/Scripts/Pause.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour
+{
+    [SerializeField] private GameObject PauseButton;
+    [SerializeField] private GameObject MenuPause;
+    private bool keyPause=false;
+
+    public void PauseGame() {
+
+        keyPause = true;
+        Time.timeScale = 0f;
+        PauseButton.SetActive(false);
+        MenuPause.SetActive(true);
+
+    }
+
+    public void resume()
+    {
+        keyPause = false;
+        Time.timeScale = 1f;
+        PauseButton.SetActive(true);
+        MenuPause.SetActive(false);
+    }
+
+    public void exitGame() {
+
+        Debug.Log("El juego se ha cerrado");
+        Application.Quit();
+    }
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+
+            if (keyPause)
+            {
+                resume();
+            }
+            else {
+
+                PauseGame();
+            }
+        }
+    }
+}
