40ba74482efac67a4dbed7dce3e1602900573ace
jnuneznunUOC <jnuneznun@uoc.edu>
2022-12-16 01:42:45 +0100

    16 diciembre
    Archivos para PEC03, corrección de errores, pantalla de game over, menu settings y compilaciones windows-macos

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,14 +5,36 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+     void Start()
+    {
+        Time.timeScale = 1;
+    }
     public void Play()
     {
         SceneManager.LoadScene(1);
+
+    }
+
+    public void returnMenu()
+        {
+        SceneManager.LoadScene(0);
+        }
+
+    public void settings() {
+
+        SceneManager.LoadScene(5);
     }
 
     public void Exit() {
 
         Application.Quit();
     }
+    public void levelWood() {
 
+        SceneManager.LoadScene(3);
+    }
+    public void levelCave() {
+
+        SceneManager.LoadScene(4);
+    }
 }
--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -4,76 +4,78 @@
     using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
-    {
+{
 
 
-        public float health;
-        public float maxh;
-        bool inmun;
-        public float inmunityTime;
-        blink material;
-        SpriteRenderer sprite;
-        public float goBackHitX;
-        public float goBackHitY;
-        public Image healthBar;
-        Rigidbody2D rg;
-        public static PlayerHealth instance;
-
+    public float health;
+    public float maxh;
+    bool inmun;
+    public float inmunityTime;
+    blink material;
+    SpriteRenderer sprite;
+    public float goBackHitX;
+    public float goBackHitY;
+    public Image healthBar;
+    Rigidbody2D rg;
+    public static PlayerHealth instance;
+    public GameObject GameOverScreen;
 
     // Start is called before the first frame update
     void Start()
-        {
-            health = maxh;
-            material = GetComponent<blink>();
-            sprite = GetComponent<SpriteRenderer>();
-            rg = GetComponent<Rigidbody2D>();
-        }
+    {
+        GameOverScreen.SetActive(false);
+        health = maxh;
+        material = GetComponent<blink>();
+        sprite = GetComponent<SpriteRenderer>();
+        rg = GetComponent<Rigidbody2D>();
+    }
 
-        // Update is called once per frame
-        void Update()
-        {
+    // Update is called once per frame
+    void Update()
+    {
         healthBar.fillAmount = health / 100;
-            if (health > maxh) {
+        if (health > maxh) {
 
-                health = maxh;
-
-            }
-        }
-        private void OnTriggerEnter2D(Collider2D collision)
-        {
-            if (collision.CompareTag("enemy")&& !inmun) {
-
-                health -= collision.GetComponent<EnemyScript>().damage;
-                StartCoroutine(Inmunnity());
-
-                if (collision.transform.position.x > transform.position.x)
-                {
-
-                    rg.AddForce(new Vector2(-goBackHitX, goBackHitY), ForceMode2D.Force);
-                }
-                else
-                {
-
-                    rg.AddForce(new Vector2(goBackHitX, goBackHitY), ForceMode2D.Force);
-
-                }
-
-
-                if (health <= 0) {
-
-
-                    //PONER REINICIO O PANTALLA MUERTE
-                    print("jugador muerto");
-                }
-            }
-        }
-        IEnumerator Inmunnity() {
-
-            inmun = true;
-            sprite.material = material.blinks;
-            yield return new WaitForSeconds(inmunityTime);
-            inmun = false;
-            sprite.material = material.original;
+            health = maxh;
 
         }
     }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("enemy") && !inmun) {
+
+            health -= collision.GetComponent<EnemyScript>().damage;
+            StartCoroutine(Inmunnity());
+
+            if (collision.transform.position.x > transform.position.x)
+            {
+
+                rg.AddForce(new Vector2(-goBackHitX, goBackHitY), ForceMode2D.Force);
+            }
+            else
+            {
+
+                rg.AddForce(new Vector2(goBackHitX, goBackHitY), ForceMode2D.Force);
+
+            }
+
+
+            if (health <= 0) {
+
+                GameOverScreen.SetActive(true);
+
+            }
+        }
+    }
+    IEnumerator Inmunnity() {
+
+        inmun = true;
+        sprite.material = material.blinks;
+        yield return new WaitForSeconds(inmunityTime);
+        inmun = false;
+        sprite.material = material.original;
+
+    }
+
+
+}
